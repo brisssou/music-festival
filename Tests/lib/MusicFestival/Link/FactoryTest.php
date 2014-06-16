730d378cb26ec77cece77e47d52690af9e8d19cd
Brice Laurencin <brice.laurencin@gmail.com>
2014-06-16 22:03:18 +0200

    trying to get test to run

--- Tests/lib/MusicFestival/Link/FactoryTest.php
+++ Tests/lib/MusicFestival/Link/FactoryTest.php
@@ -23,7 +23,7 @@ class FactoryTest extends \PHPUnit_Framework_TestCase {
    * @todo Implement testFromUrl().
    */
   public function testFromUrl() {
-    $linkClass = $this->getMockClass('Link');
+    $linkClass = $this->getMockClass('Link', 'isMatchingUrl');
 
     $linkClass::staticExpects($this->any())
           ->method('isMatchingUrl')
